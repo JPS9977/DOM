a8c8697f8875f40a7eb2c6ca08d0ddd3c8652b8a
JPS9977 <juampy9977@gmail.com>
2024-11-21 17:04:57 -0300

    first commit

---
+++ apuntes/main.js
@@ -0,0 +1,53 @@
+// const menu = document.querySelector('#links')
+
+const image = document.querySelector('nav>img')
+// console.log(image.alt)
+image.alt = 'Icono'
+
+const link1 = document.querySelector('#link1')
+console.log(link1);
+link1.innerText = 'HOME'
+
+link1.innerHTML = '<p>Home</p>'
+
+//Cambiar estilos del elemento con la propiedad style
+
+link1.style.color = 'red'
+
+const list = document.querySelector('#links')
+
+console.log(list);
+//Crear elemento
+const li = document.createElement('li')
+//Agregamos texto o atributos 
+li.innerText = 'About'
+li.setAttribute('id', 'ultimo link')
+//agregamos el elemento al contenedor padre
+
+list.append(li)
+
+const sectionProductos = document.querySelector('.productos')
+console.log(sectionProductos);
+
+sectionProductos.classList.add('fondo-verde')
+// sectionProductos.classList.remove('fondo-verde')
+// sectionProductos.classList.toggle('fondo-verde')
+
+console.log(sectionProductos.classList.contains('fondo-verde'));
+
+//contenedor
+const header = document.querySelector('header')
+//elemento
+const boton = document.createElement('button')
+
+boton.innerText = 'MENÚ'
+//asociar el elemento
+header.append(boton)
+
+boton.addEventListener('click', ()=>{
+    console.log('BOTÓN PRESIONADO');
+})
+
+// EJEMPLO
+// ALTERNAR LOS ESTILOS DE UN ELEMENTO DEL HTML CON UN BOTÓN
+
